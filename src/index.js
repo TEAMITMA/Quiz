f6e2100380018ecfdebaeb405a1fc23a1d88d93c
Muhammad Mubeen <76704077+muhammadmubeen12345@users.noreply.github.com>
2021-03-28 00:47:18 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ class App extends React.Component {
             {
                 (name && email)?
                     <Main/>:
+
                     <Form/>
             }
             <Footer/>
