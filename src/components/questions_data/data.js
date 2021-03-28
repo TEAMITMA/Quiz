d1c60febf910e7decde4c552ca5defa28ee47041
“mderouich@vmware.com” <mderouich@vmware.com>
2021-03-28 12:03:48 +0200

    try to fix quiz

--- src/components/questions_data/data.js
+++ src/components/questions_data/data.js
@@ -1,16 +1,5 @@
 export default [
-    {
-        question: 'Pour tout a de]0 ;π[ on a :  lim┬(t→a)  1/tsint=1/asina ',
-        questionAR: ' لكل عنصر a من ]0 ;π[  لدينا lim┬(t→a)  1/tsint=1/asina',
 
-        answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 1
-    },
     {
         question: 'Pour tout a de]0 ;π[ on a :  lim┬(t→a)  1/tsint=1/asina ',
         questionAR: ' لكل عنصر a من ]0 ;π[  لدينا lim┬(t→a)  1/tsint=1/asina',
