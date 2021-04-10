e7c57a3434cd2a5b06713f89eefa8cf1caf4f4a0
Muhammad Mubeen <76704077+muhammadmubeen12345@users.noreply.github.com>
2021-04-10 14:00:25 +0500

    updated question data

--- src/components/questions_data/data.js
+++ src/components/questions_data/data.js
@@ -4,7 +4,7 @@ export default [
         question: 'La fonction f définie sur ]- ∞;0[∪]0;4]\n' +
             'Par f(x)=(x-2)/√(4-x-2) est dérivable en 2\n',
         questionAR: 'الدالة f المعرفة على المجال    ]- ∞;0[∪]0;4] ب :\n' +
-            ' f(x)=(x-2)/√(4-x-2)  قابلة للإشتقاق في  \n',
+            ' f(x)=(x-2)/√(4-x-2)  قابلة للإشتقاق في2  \n',
         answers: [
             'Vrai / صحيح',
             'Faux / خطأ',
@@ -29,7 +29,7 @@ export default [
     {
         //3
         question: 'Pour tout réel m de l’intervalle]-2 ;2[, l’équation x³-3x = m admet deux solutions',
-        questionAR: 'لكل عدد حقيقي m ينتمي للمجال , ]-2 ;2[ المعادلة x³ - 3x = m \n' +
+        questionAR: 'لكل عدد حقيقي m ينتمي للمجال ]-2 ;2[ المعادلة x³ - 3x = m \n' +
             'تقبل حلين إثنين\n',
         answers: [
             'Vrai / صحيح',
@@ -53,13 +53,13 @@ export default [
     },
     {
         //5
-        question: 'lim┬(x→+∞)⁡〖f(x)-x=1〗' +
+        question: 'Si lim┬(x→+∞)⁡f(x)-x=1' +
             '\n Alors ',
-        questionAR : 'إذا كانت lim┬(x→+∞)⁡〖f(x)-x=1〗  \n' +
+        questionAR : 'إذا كانت lim┬(x→+∞)⁡f(x)-x=1  \n' +
             'إذن :\n',
         answers: [
             '(Cf) admet une asymptote oblique d’équation (يقبل مقاربا مائلا معادلته ) y=-x+1',
-            '(Cf) admet une asymptote oblique d’équation   يقبل مقاربا مائلا معادلته) )  y= x-1',
+            '(Cf) admet une asymptote oblique d’équation   (يقبل مقاربا مائلا معادلته)  y= x-1',
             '(Cf) admet une asymptote oblique d’équation (يقبل مقاربا مائلا معادلته   )  y= x+1',
             '(Cf) admet une asymptote oblique d’équation (يقبل مقاربا مائلا معادلته    ) y= 1'
         ],
@@ -68,13 +68,13 @@ export default [
     {
         //6
         question: 'La fonction dérivée de la fonction\n' +
-            ' f:x→x²〖e ̄〗^x est définie par :\n',
-        questionAR: "الدالة المشتقة للدالة f:x→x²〖e ̄〗^x  معرفة بما يلي :",
+            ' f:x→x²e^(-x) est définie par :\n',
+        questionAR: "الدالة المشتقة للدالة f:x→x²e^(-x)  معرفة بما يلي :",
         answers: [
-            'f^\' (x)=-2xe^(-x)',
-            'f^\' (x)=2xe^(-x)',
-            'f^\' (x)=x(2-x)e^(-x)',
-            'f^\' (x)=x(2+x)e^(-x)'
+            'f\'(x)=-2xe^(-x)',
+            'f\'(x)=2xe^(-x)',
+            'f\'(x)=x(2-x)e^(-x)',
+            'f\'(x)=x(2+x)e^(-x)'
         ],
         correct: 3
     },
@@ -200,8 +200,8 @@ export default [
     },
     {
         //16
-        question: 'la fonction f : x→¬ | x²-9|',
-        questionAR : 'لدالة f : x→¬ | x²-9|',
+        question: 'la fonction f : x→ | x²-9|',
+        questionAR : 'الدالة f : x→|x²-9|',
         answers: [
             'Est Continue sur (متصلة على) ]- ∞;-3] et [3;+∞[ ',
             'b)\tEst non continue en (غير متصلة على) 3' ,
@@ -229,7 +229,7 @@ export default [
             'lim┬(x→x₀)⁡〖f(x)=f(x₀)〗',
             'lim┬(x→x₀)⁡〖(f(x)-f(x₀))/(x-x₀)=l     ;     l∈R〗',
             'lim┬(x→x₀)⁡〖(f(x)-f(x₀))/(x-x₀)=+ ∞〗',
-            'lim┬(x→x₀)⁡〖f(x₀+h)/h=l    ;l〗∈R'
+            'lim┬(x→x₀)⁡ (f(x₀+h)/h)=l   ;l∈R'
         ],
         correct: 2
     },
@@ -264,7 +264,7 @@ export default [
     {
         //21
         question: 'Si ln(2x- 1) = ln(x-1) alors x=0',
-        questionAR : 'إذا كانت ln(2x- 1) = ln(x-1) alors x=0\n' +
+        questionAR : 'إذا كانت ln(2x- 1) = ln(x-1) \n' +
             'إذا x=0\n',
         answers: [
             'Vrai / صحيح',
@@ -277,22 +277,22 @@ export default [
     {
         //22
         question: 'soit f  une fonction définie par\n' +
-            '   f(x)=x^7+ 7 x\n' +
+            '   f(x)=x^7+ 7x\n' +
             'On a :\n',
         questionAR : "لتكن الدالة f معرفة بما يلي:\n" +
-            "   f(x)=x^7+ 7 x\n",
+            "   f(x)=x^7+ 7x\n",
         answers: [
-            'lim┬(n→+∞)⁡〖f(x)=〗 +∞',
-            'lim┬(n→-∞)⁡〖f(x)=〗  -∞',
-            'lim┬(n→+∞)⁡〖f(x)=〗 -∞',
-            'lim┬(n→-∞)⁡〖f(x)=〗  +∞'
+            'lim┬(n→+∞)⁡  f(x)= +∞',
+            'lim┬(n→-∞)⁡f(x)= -∞',
+            'lim┬(n→+∞)⁡f(x)= -∞',
+            'lim┬(n→-∞)⁡f(x)= +∞'
         ],
         correct: 1
     },
     {
         //23
-        question: 'L’ensemble des solutions de l’inéquation     xln(0,2) -5≥0 est∶',
-        questionAR:'مجموعة حلول المتراجحة xln(0,2) -5≥0' ,
+        question: 'L’ensemble des solutions de l’inéquation     xln(0,2)-5≥0 est∶',
+        questionAR:'مجموعة حلول المتراجحة xln(0,2)-5≥0' ,
         answers: [
             '[5/(ln⁡(0,2));0[',
             '[-∞;5/(ln⁡(0,2))[',
@@ -368,7 +368,7 @@ export default [
     {
         //29
         question: 'Si  (f  ̄¹)’(x₀) existe alors elle est égale à :',
-        questionAR : 'إذا كانت (f  ̄¹)’(x₀) موجودة إذن تعادل ',
+        questionAR : 'إذا كانت (f ̄¹)’(x₀) موجودة إذن تعادل ',
         answers: [
             '1/(f\'(f(x₀))',
             '1/(f\'of(x₀))',
