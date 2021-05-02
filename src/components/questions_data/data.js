ae3a3beb5f11e7fd9a82c3787f1b4f6052b25b6f
Muhammad Mubeen <76704077+muhammadmubeen12345@users.noreply.github.com>
2021-05-02 15:25:13 +0500

    questions modified

--- src/components/questions_data/data.js
+++ src/components/questions_data/data.js
@@ -18,7 +18,7 @@ export default [
     {
         //2
         question: 'La suite (Un) définie sur N par Un =(n³-n)/5 est croissante',
-        questionAR: '   تزايدية      Un =(n³-n)/5   يلي  بما N المتتالية المعرفة على      ',
+        questionAR: 'المتتالية المعرفة على N بما يلي Un =(n^3-n)/5     تزايدية     ',
         answers: [
             'Vrai / صحيح',
             'Faux / خطأ',
@@ -44,9 +44,7 @@ export default [
         //4
         question: 'la suite (Wn) définie sur N par \n' +
             'Wn = n-4√(n+1) est divergente\n',
-        questionAR : ' كالتالي  N  المعرفة على (Wn ) المتتالية\n' +
-            'Wn = n-4√(n+1) \n' +
-            'متتالية متباعدة\n',
+        questionAR : 'المتتالية(Wn )   المعرفة على  Nكالتالي   Wn = n-4√(n+1)متتالية متباعدة',
         answers: [
             'Vrai / صحيح',
             'Faux / خطأ',
@@ -59,341 +57,337 @@ export default [
         //5
         question: 'Soit (Un) et (Vn) deux suites numériques qui vérifient : ∀ n∈N Un≤ Vn  ',
         questionAR : '\n' +
-            'متتاليتان عدديتان تحققان (Vn) و (Un) لتكن\n' +
-            '∀ n∈N Un≤ Vn  \n' +
-            'Alors :\n',
-        answers: [
-            'Si (Un) diverge(متباعدة) alors (Vn) diverge(متباعدة)',
-            'Si (Vn) est bornée(محدودة) alors (Un) est majorée(مكبورة)',
-            'la suite (Un) est majorée(مكبورة)',
-            'la (Vn) est majorée (مكبورة)'
-        ],
-        correct: 2
-    },
-    {
-        //6
-        question: 'Soit  f   la fonction définie par :\n' +
-            ' f :x→cos⁡〖(πx/(x²+1)〗)\n',
+            'لتكن (Vn) و (Un) متتاليتان عدديتان تحققان ∀ n∈N Un≤ Vn',
+answers: [
+    'Si (Un) diverge(متباعدة) alors (Vn) diverge(متباعدة)',
+    'Si (Vn) est bornée(محدودة) alors (Un) est majorée(مكبورة)',
+    'la suite (Un) est majorée(مكبورة)',
+    'la (Vn) est majorée (مكبورة)'
+],
+    correct: 2
+},
+{
+    //6
+    question: 'Soit  f   la fonction définie par :\n' +
+    ' f :x→cos⁡〖(πx/(x²+1)〗)\n',
         questionAR: "لتكن f الدالة المعرفة كما يلي f :x→cos⁡〖(πx/(x²+1)〗)",
+    answers: [
+    'f  est continue en (في  متصلة) 1',
+    'f n\' est pas continue en (في  متصلة غير ) R',
+    'f n\' est pas continue en (في  متصلة غير ) 1',
+    'f n\' est pas continue en (في  متصلة غير ) 01/2'
+],
+    correct: 1
+},
+{
+    //7
+    question: 'A = (√(15&3^5 )  ×∛(9 )×√(10&15))/√(5&3) est égale à(تساوي):',
         answers: [
-            'f  est continue en (في  متصلة) 1',
-            'f n^\' est pas continue en (في  متصلة غير ) R',
-            'f n^\' est pas continue en (في  متصلة غير ) 1',
-            'f n^\' est pas continue en (في  متصلة غير ) 01/2'
-        ],
-        correct: 1
-    },
-    {
-        //7
-        question: 'A = (√(15&3^5 )  ×∛(9 )×√(10&15))/√(5&3) est égale à(تساوي):',
-        answers: [
-            '15',
-            '3',
-            '9',
-            '1'
-        ],
-        correct: 3
-    },
-    {
-        //8
-        question: 'Soit f la fonction définie par :\t\n' +
-            '\tf(x)=x+√(x+3) \n' +
-            'on a :\n',
+    '15',
+    '3',
+    '9',
+    '1'
+],
+    correct: 3
+},
+{
+    //8
+    question: 'Soit f la fonction définie par :\t\n' +
+    '\tf(x)=x+√(x+3) \n' +
+    'on a :\n',
         questionAR : 'لتكن f الدالة المعرفة كما يلي f(x)= x+√(x+3)',
-        answers: [
-            'f est continue et strictement croissante  en ( متصلة و تزايدية قطعا في)  Df',
-            'fn’est pas continue en (غير متصلة في) Df',
-            'f est continue et strictement décroissante (متصلة و تناقصية قطعا في) en Df',
-            'f est décroissante en (تناقصية في) Df'
-        ],
-        correct: 1
-    },
-    {
-        //9
-        question: 'L’image de l’intervalle [0 ; 4] par la fonction :\n' +
-            '  g: x→12x-√x est l’intervalle [0 ; 9]\n',
+    answers: [
+    'f est continue et strictement croissante  en ( متصلة و تزايدية قطعا في)  Df',
+    'fn’est pas continue en (غير متصلة في) Df',
+    'f est continue et strictement décroissante (متصلة و تناقصية قطعا في) en Df',
+    'f est décroissante en (تناقصية في) Df'
+],
+    correct: 1
+},
+{
+    //9
+    question: 'L’image de l’intervalle [0 ; 4] par la fonction :\n' +
+    '  g: x→12x-√x est l’intervalle [0 ; 9]\n',
         questionAR : 'صورة المجال [0,4] بالدالة  g: x→12x-√x\n' +
-            'هو المجال  [0 ; 9]\n',
+'هو المجال  [0 ; 9]\n',
+    answers: [
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 2
+},
+{
+    //10
+    question: '√(√(√(10 )) )  = ∛10',
         answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 2
-    },
-    {
-        //10
-        question: '√(√(√(10 )) )  = ∛10',
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 2
+},
+{
+    //11
+    question: 'B= x³+3x²+3x+9 ',
         answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 2
-    },
-    {
-        //11
-        question: 'B= x³+3x²+3x+9 ',
-       answers: [
-            'B= (x+1)³ +9',
-            'B= (x+1)³ +1',
-            'B= (x+1)³ ',
-            'B= (x+1)³ +8'
-        ],
-        correct: 4
-    },
-    {
-        //12
-        question: 'l^\' équation ln⁡〖(x-1)²=2ln3 admet〗 deux solutions :',
-        questionAR : 'المعادلة ln⁡〖(x-1)²=2ln3 〗تقبل حلين إثنين' ,
+    'B= (x+1)³ +9',
+    'B= (x+1)³ +1',
+    'B= (x+1)³ ',
+    'B= (x+1)³ +8'
+],
+    correct: 4
+},
+{
+    //12
+    question: 'l\'équation ln⁡(x-1)²=2ln3 admet deux solutions :',
+    questionAR : 'المعادلة ln⁡(x-1)²=2ln3 تقبل حلين إثنين' ,
         answers: [
-            '{1; 3}',
-            '{-2; 4}',
-            '{-2 ; 1}',
-            '{1 ; 4}'
-        ],
-        correct: 2
-    },
-    {
-        //13
-        question: 'L’équation √(5&3x-4)=2 admet une seule solution :',
-        questionAR:'وحيد  حل تقبل   √(5&3x-4)=2 المعادلة ',
-        answers: [
-            '10',
-            '11',
-            '12',
-            '13'
-        ],
-        correct: 3
-    },
-    {
-        //14
-        question: 'l’équation x⁶=3\n' +
-            ' Admet dans R\n',
+    '{1; 3}',
+    '{-2; 4}',
+    '{-2 ; 1}',
+    '{1 ; 4}'
+],
+    correct: 2
+},
+{
+    //13
+    question: 'L’équation √(5&3x-4)=2 admet une seule solution :',
+        questionAR:'المعادلة√(5&3x-4)=2   تقبل حل وحيد  ',
+    answers: [
+    '10',
+    '11',
+    '12',
+    '13'
+],
+    correct: 3
+},
+{
+    //14
+    question: 'l’équation x⁶=3\n' +
+    ' Admet dans R\n',
         questionAR : 'المعادلة x⁶=3\n' +
-            'تقبل في  R\n',
-        answers: [
-            'Une seule solution || حل وحيد',
-            'Deux solutions || حلان اثنان',
-            'Trois solutions || ثلاث حلول',
-            'Six solutions/ حلول ستة '
-        ],
-        correct: 2
-    },
-    {
-        //15
-        question: 'Soit f  la fonction définie]-3;3[ par :\n' +
-            '          f(x) =x√(9-x²)\n',
+'تقبل في  R\n',
+    answers: [
+    'Une seule solution || حل وحيد',
+    'Deux solutions || حلان اثنان',
+    'Trois solutions || ثلاث حلول',
+    'Six solutions/ حلول ستة '
+],
+    correct: 2
+},
+{
+    //15
+    question: 'Soit f  la fonction définie]-3;3[ par :\n' +
+    '          f(x) =x√(9-x²)\n',
         questionAR : 'لتكن f دالة معرفة على ]-3 ;3[بما يلي\n' +
-            '          f(x) =x√(9-x²)\n',
+'          f(x) =x√(9-x²)\n',
+    answers: [
+    'f  est impaire (فردية)',
+    'f est paire (زوجية)',
+    'f est croissante (تصاعدية )',
+    'f est décroissante (تنازلية)'
+],
+    correct: 1
+},
+{
+    //16
+    question: 'Soit f  la fonction f : x→ (x-1)√(3x²+x)\n' +
+    'F est dérivable sur R*+ et sa dérivée :\n',
+        questionAR : 'الدالة f : x→ (x-1)√(3x²+x) تقبل  الاشتقاق و مشتقتها على R*+',
+    answers: [
+    '(12x²-3x-1)/√(3x²+x)',
+    '(x²-3x-1)/(2√(3x²+x))' ,
+    '(12x²-3x)/(2√(3x²+x))',
+    '(12x²-3x-1)/(2√(3x²+x))'
+],
+    correct: 4
+},
+{
+    //17
+    question: 'lim┬(x→+∞) x-√((x+1)/x)=0',
         answers: [
-            'f  est impaire (فردية)',
-            'f est paire (زوجية)',
-            'f est croissante (تصاعدية )',
-            'f est décroissante (تنازلية)'
-        ],
-        correct: 1
-    },
-    {
-        //16
-        question: 'Soit f  la fonction f : x→¬ (x-1)√(3x²+x)\n' +
-            'F est dérivable sur R*+ et sa dérivée :\n',
-        questionAR : 'الدالة f : x→¬ (x-1)√(3x²+x) تقبل  الاشتقاق و مشتقتها على R*+',
-        answers: [
-            '(12x²-3x-1)/√(3x²+x)',
-            '(x²-3x-1)/(2√(3x²+x))' ,
-            '(12x^2-3x)/(2√(3x^2+x))',
-            '(12x²-3x-1)/(2√(3x²+x))'
-        ],
-        correct: 4
-    },
-    {
-        //17
-        question: 'lim┬(x→+∞) x-√((x+1)/x)=0',
-        answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 1
-    },
-    {
-        //18
-        question: 'La dérivée de la fonction  f définie sur\n' +
-            ' [0 ; +∞[ par : f(x)=(x-√x)/(x+√x) est :\n',
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 1
+},
+{
+    //18
+    question: 'La dérivée de la fonction  f définie sur\n' +
+    ' [0 ; +∞[ par : f(x)=(x-√x)/(x+√x) est :\n',
         questionAR : 'الدالة المشتقة ل f و المعرفة في  [0 ; +∞[\n' +
-            'بما يلي f(x)=(x-√x)/(x+√x)هي\n',
-        answers: [
-            'f^\' (x)=√x/(x+√x)',
-            'f^\' (x)=x/(x+√x)',
-            'f^\' (x)=x/((x+√(x)²))',
-            'f^\' (x)=√x/((x+√(x)²))'
-        ],
-        correct: 4
-    },
-    {
-        //19
-        question: 'Soit f  une  fonction définie par\n' +
-            '   f(x)=  2/(2-lnx)\n' +
-            'On a :\n',
+'بما يلي f(x)=(x-√x)/(x+√x)هي\n',
+    answers: [
+    'f\' (x)=√x/(x+√x)',
+    'f\' (x)=x/(x+√x)',
+    'f\' (x)=x/((x+√(x)²))',
+    'f\' (x)=√x/((x+√(x)²))'
+],
+    correct: 4
+},
+{
+    //19
+    question: 'Soit f  une  fonction définie par\n' +
+    '   f(x)=  2/(2-lnx)\n' +
+    'On a :\n',
         questionAR : 'لتكن الدالة f معرفة بما يلي: \n' +
-            '   f(x)=  2/(2-lnx)\n',
-        answers: [
-            'Df   = R',
-            'Df   = R*',
-            'Df   = ]0 ;e²[∪]e²;+∞[',
-            'Df   = R –{2}'
-        ],
-        correct: 3
-    },
-    {
-        //20
-        question: 'Si  ln(2x-1) = ln(x-1) alors x= 0',
+'   f(x)=  2/(2-lnx)\n',
+    answers: [
+    'Df   = R',
+    'Df   = R*',
+    'Df   = ]0 ;e²[∪]e²;+∞[',
+    'Df   = R –{2}'
+],
+    correct: 3
+},
+{
+    //20
+    question: 'Si  ln(2x-1) = ln(x-1) alors x= 0',
         questionAR : 'إذا كانت ln(2x-1) = ln(x-1) إذن x= 0',
-        answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 2
-    },
-    {
-        //21
-        question: 'soit f  une fonction définie par\n' +
-            '   f(x)=-1/2+ x/(2√(x²+1))\n',
+    answers: [
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 2
+},
+{
+    //21
+    question: 'soit f  une fonction définie par\n' +
+    '   f(x)=-1/2+ x/(2√(x²+1))\n',
         questionAR : 'لتكن الدالة f معرفة بما يلي:\n' +
-            '   f(x)=-1/2+ x/(2√(x²+1))\n',
+'   f(x)=-1/2+ x/(2√(x²+1))\n',
+    answers: [
+    'lim┬(n→+∞)⁡f(x)= +∞',
+    'lim┬(n→-∞)⁡f(x)= -∞',
+    'lim┬(n→+∞)⁡f(x)= 0',
+    'lim┬(n→-∞)⁡f(x)= 0'
+],
+    correct: 3
+},
+{
+    //22
+    question: 'lim┬(x→+∞)⁡〖x-√((x+1)/x)〗 n’existe pas (غير موجود)',
         answers: [
-            'lim┬(n→+∞)⁡f(x)= +∞',
-            'lim┬(n→-∞)⁡f(x)= -∞',
-            'lim┬(n→+∞)⁡f(x)= 0',
-            'lim┬(n→-∞)⁡f(x)= 0'
-        ],
-        correct: 3
-    },
-    {
-        //22
-        question: 'lim┬(x→+∞)⁡〖x-√((x+1)/x)〗 n’existe pas (غير موجود)',
-        answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 2
-    },
-    {
-        //23
-        question: 'la fonction f définie sur ]0 ;+ ∞[ par\n' +
-            ' f(x)=∛(x²+x+1)+1/(x²+1) n’admet pas une primitive \n',
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 2
+},
+{
+    //23
+    question: 'la fonction f définie sur ]0 ;+ ∞[ par\n' +
+    ' f(x)=∛(x²+x+1)+1/(x²+1) n’admet pas une primitive \n',
         questionAR:'الدالة f المعرفة على المجال]0 ;+ ∞[    ب :\n' +
-            'f(x)=∛(x²+x+1)+1/(x²+1)  \n' +
-            'لا تقبل دالة أصلية\n' ,
-        answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 2
-    },
-    {
-        //24
-        question: 'Soit f la fonction définie par f(x) =x√(4-x²)\n' +
-            'Elle admet le point O(0 ;0) comme point d’inflexion de (Cf)\n',
-        questionAR : ': معرفة بما يلي الدالة f    \n' +
-            'f(x) =x√(4-x^2 ) \n' +
-            'تقبل نقطة انعطاف\n',
-        answers:[
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 1
-    },
-    {
-        //25
-        question: 'Toute fonction continue sur I admet  une primitive sur I',
+'f(x)=∛(x²+x+1)+1/(x²+1)  \n' +
+'لا تقبل دالة أصلية\n' ,
+    answers: [
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 2
+},
+{
+    //24
+    question: 'Soit f la fonction définie par f(x) =x√(4-x²)\n' +
+    'Elle admet le point O(0 ;0) comme point d’inflexion de (Cf)\n',
+        questionAR : 'الدالة f  معرفة بما يلي    f(x) =x√(4-x^2 ) تقبل نقطة انعطاف',
+    answers:[
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 1
+},
+{
+    //25
+    question: 'Toute fonction continue sur I admet  une primitive sur I',
         questionAR : 'كل دالة متصلة على مجال I  تقبل دالة أصلية على نفس هذا المجال',
-        answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 1
-    },
-    {
-        //26
-        question: 'ln⁡(x-1)-ln⁡(3x+2)<0 \n' +
-            'admet des solutions dans R\n',
-        questionAR : '〖 ln〗⁡(x-1)-ln⁡(3x+2)<0    المتراجحة \n' +
-            'تقبل حلولا في \n' +
-            'R  \t\t     \n',
-        answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 1
-    },
-    {
-        //27
-        question: 'L’ensemble de solutions de l’équation \n' +
-            'مجموعة حلول المعادلة \n' +
-            'log₃(2x-1) – log₃(x) =1   est هو :\n',
-        answers: [
-            'R',
-            ']1/2; ++∞[',
-            '∅',
-            'R+'
-        ],
-        correct: 3
-    },
-    {
-        //28
-        question: 'l’ensemble de définition de l’équation\n' +
-            'f(x) =(1+lnx)/(x(lnx)²)\n',
+    answers: [
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 1
+},
+{
+    //26
+    question: 'ln⁡(x-1)-ln⁡(3x+2)<0 \n' +
+    'admet des solutions dans R\n',
+        questionAR : 'المتراجحة〖 ln〗⁡(x-1)-ln⁡(3x+2)<0     تقبل حلولا في R' +
+'R  \t\t     \n',
+    answers: [
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 1
+},
+{
+    //27
+    question: 'L’ensemble de solutions de l’équation \n',
+        questionAR :  'مجموعة حلول المعادلة log₃(2x-1) – log₃(x) =1   هو ',
+    answers: [
+    'R',
+    ']1/2; ++∞[',
+    '∅',
+    'R+'
+],
+    correct: 3
+},
+{
+    //28
+    question: 'l’ensemble de définition de l’équation\n' +
+    'f(x) =(1+lnx)/(x(lnx)²)\n',
         questionAR :'مجموعة تعريف المعادلة\n' +
-            'f(x) =(1+lnx)/(x(lnx)²)\n',
-        answers: [
-            'L’ensemble vide (مجموعة فارغة)',
-            'R',
-            ']0 ;1[∪]1+∞[ ',
-            'R*'
-        ],
-        correct: 3
-    },
-    {
-        //29
-        question: 'La limite de la suite نهاية المتتالية\n' +
-            'Un= sin⁡n/(n+2) est هو:\n',
-        answers: [
-            'lim┬(n→+∞)⁡〖Un=〗  0',
-            'lim┬(n→+∞)⁡〖Un=〗  +∞',
-            'lim┬(n→+∞)⁡〖Un=〗  1',
-            'lim┬(n→+∞)⁡〖Un=〗  2'
-        ],
-        correct: 1
-    },
-    {
-        //30
-        question: 'Ω(a,b)est un centre de symétrie de (Cf)si :\n' +
-            'اذا(Cf)مركز تماثل Ω(a,b)\n' +
-            '∀ x∈Df (2a-x)∈Df\n' +
-            'et f(2a-x)+f(x)=2b\n',
-        answers: [
-            'Vrai / صحيح',
-            'Faux / خطأ',
-            '',
-            ''
-        ],
-        correct: 1
-    }
+'f(x) =(1+lnx)/(x(lnx)²)\n',
+    answers: [
+    'L’ensemble vide (مجموعة فارغة)',
+    'R',
+    ']0 ;1[∪]1+∞[ ',
+    'R*'
+],
+    correct: 3
+},
+{
+    //29
+    question: 'La limite de la suite نهاية المتتالية\n' +
+    'Un= sin⁡n/(n+2) est هو:\n',
+        questionAR : 'نهاية المتتالية Un= sin⁡n/(n+2) هو :',
+    answers: [
+    'lim┬(n→+∞)⁡ Un= 0',
+    'lim┬(n→+∞)⁡Un= +∞',
+    'lim┬(n→+∞)⁡Un= 1',
+    'lim┬(n→+∞)⁡Un= 2'
+],
+    correct: 1
+},
+{
+    //30
+    question: 'Ω(a,b)est un centre de symétrie de (Cf)si :\n' +
+    'اذا(Cf)مركز تماثل Ω(a,b)\n' +
+    '∀ x∈Df (2a-x)∈Df\n' +
+    'et f(2a-x)+f(x)=2b\n',
+        questionAR :'نهاية المتتالية Un= sin⁡n/(n+2) هو :',
+    answers: [
+    'Vrai / صحيح',
+    'Faux / خطأ',
+    '',
+    ''
+],
+    correct: 1
+}
 ]
